5116ddd647196472a5b08ee25053e958fb7224d7
Oleg Akopov <oleg.akopov@undisker.com>
2026-01-29 20:12:31 +0100

    cleanup-1

--- Scripts/BuildDoc.bat
+++
@@ -1,7 +0,0 @@
-@echo OFF
-
-echo Building XHTML documentation
-..\Bin\DracoolaDoc -doc -f=xhtml -i=..\Doc\DracoolaDoc\Imaging.vdocproj -o=..\Doc
-
-echo Building HTMLHelp documentation
-..\Bin\DracoolaDoc -doc -f=htmlhelp -i=..\Doc\DracoolaDoc\Imaging.vdocproj -o=..\Doc
